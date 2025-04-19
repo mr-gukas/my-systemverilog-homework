dd7de036569a21d0fed976ebb0509fcbb9f58cb6
mr-gukas <gukasyan.vladimir@mail.ru>
2025-04-19 23:48:13 +0300

    add 4_11

--- 04_arithmetics_and_pipelining/04_11_sqrt_formula_distributor/04_11_sqrt_formula_distributor.sv
+++ 04_arithmetics_and_pipelining/04_11_sqrt_formula_distributor/04_11_sqrt_formula_distributor.sv
@@ -12,8 +12,8 @@ module sqrt_formula_distributor
     input  [31:0] b,
     input  [31:0] c,
 
-    output        res_vld,
-    output [31:0] res
+    output logic        res_vld,
+    output logic [31:0] res
 );
 
     // Task:
@@ -43,5 +43,72 @@ module sqrt_formula_distributor
     // Instantiate sufficient number of "formula_1_impl_1_top", "formula_1_impl_2_top",
     // or "formula_2_top" modules to achieve desired performance.
 
+    localparam NUM_CHAN = (formula == 1) ? 13 : 49;
+    
+    logic [31:0] arg_in_a  [NUM_CHAN];
+    logic [31:0] arg_in_b  [NUM_CHAN];
+    logic [31:0] arg_in_c  [NUM_CHAN];
+    logic        arg_valid [NUM_CHAN];
+    
+    logic [31:0] res_out   [NUM_CHAN];
+    logic        res_valid [NUM_CHAN];
+    
+    logic [7:0] channel_idx;
+    
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            channel_idx <= 0;
+        end else begin
+            if (channel_idx == (NUM_CHAN - 1))
+                channel_idx <= 0;
+            else
+                channel_idx <= channel_idx + 1;
+        end
+    end
+    
+    always_comb begin
+        for (int j = 0; j < NUM_CHAN; j++) begin
+            arg_valid[j] = 1'b0;
+        end
+        
+        arg_in_a[channel_idx]  = a;
+        arg_in_b[channel_idx]  = b;
+        arg_in_c[channel_idx]  = c;
+        arg_valid[channel_idx] = arg_vld;
+        
+        res     = res_out[channel_idx];
+        res_vld = res_valid[channel_idx];
+    end
+
+    generate
+        genvar i;
+        if (formula == 1) begin : gen_formula1
+            for (i = 0; i < NUM_CHAN; i = i + 1) begin
+                formula_1_impl_1_top u_formula1 (
+                    .clk    (clk),
+                    .rst    (rst),
+                    .a      (arg_in_a[i]),
+                    .b      (arg_in_b[i]),
+                    .c      (arg_in_c[i]),
+                    .arg_vld(arg_valid[i]),
+                    .res_vld(res_valid[i]),
+                    .res    (res_out[i])
+                );
+            end
+        end else if (formula == 2) begin : gen_formula2
+            for (i = 0; i < NUM_CHAN; i = i + 1) begin
+                formula_2_top u_formula2 (
+                    .clk    (clk),
+                    .rst    (rst),
+                    .a      (arg_in_a[i]),
+                    .b      (arg_in_b[i]),
+                    .c      (arg_in_c[i]),
+                    .arg_vld(arg_valid[i]),
+                    .res_vld(res_valid[i]),
+                    .res    (res_out[i])
+                );
+            end
+        end
+    endgenerate
 
 endmodule
